fd725de9e4bf02eda138c3c7dda7eacf61be3bbc
cbuliarca <buliarca@yahoo.com>
2016-11-15 06:03:39 +0200

    changed persistent button

---
+++ BCM_toolsUI/extras/scripts/test_focusOnPanel.jsx
@@ -0,0 +1,12 @@
+/*
+<javascriptresource>
+  <name>BCM focus on ESHuiPanel...</name>
+  <category>BCM ESHui</category>
+</javascriptresource>
+*/
+
+
+var f = new File(Folder.temp + '/bcmPSTool.txt');
+f.open('w');
+f.write('0');
+f.close();
